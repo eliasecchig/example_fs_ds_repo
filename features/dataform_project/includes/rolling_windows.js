be3e5015b780d2bc76c576acbf968e8647a271d7
Elia Secchi <eliasecchi@google.com>
2023-02-13 18:52:58 +0000

    dataform compatibility core and cloud

--- features/dataform_project/includes/rolling_windows.js
+++ features/dataform_project/includes/rolling_windows.js
@@ -20,23 +20,23 @@ function cast_aggregation_end(aggregation) {
   }
 }
 
-function default__compute_rolling_windows(aggregations, columnsToAggregate, partition, timestampColumn, lookbackWindowsStart, excludeCurrentRow = true) {
+function default__compute_rolling_windows({ aggregations, columnsToAggregate, partition, timestampColumn, lookbackWindowsStart, excludeCurrentRow = true}) {
   const seconds_per_unit = { "s": 1, "m": 60, "h": 3600, "d": 86400, "w": 604800 };
-  partitionColumns = partition;
-  partitionColumnsName = partition.replace(',', '_').toLowerCase();
-  frameSecondsEnd = "CURRENT ROW";
-  lookbackWindowEnd = "0s";
+  let partitionColumns = partition;
+  let partitionColumnsName = partition.replace(',', '_').toLowerCase();
+  let frameSecondsEnd = "CURRENT ROW";
+  let lookbackWindowEnd = "0s";
   if (excludeCurrentRow) {
     frameSecondsEnd = "1 PRECEDING";
     lookbackWindowEnd = "1s";
   }
-  result = "";
-  for (columnToAggregate of columnsToAggregate) {
-    for (aggregation of aggregations) {
-      for (lookbackWindowStart of lookbackWindowsStart) {
-        frameSecondsStart = (parseInt(lookbackWindowStart.slice(0, -1)) * (seconds_per_unit[lookbackWindowStart[lookbackWindowStart.length - 1]]));
-        castAggregationStart = cast_aggregation_start(aggregation);
-        rollingWindow = default__rolling_window(
+  let result = "";
+  for (let columnToAggregate of columnsToAggregate) {
+    for (let aggregation of aggregations) {
+      for (let lookbackWindowStart of lookbackWindowsStart) {
+        let frameSecondsStart = (parseInt(lookbackWindowStart.slice(0, -1)) * (seconds_per_unit[lookbackWindowStart[lookbackWindowStart.length - 1]]));
+        let castAggregationStart = cast_aggregation_start(aggregation);
+        let rollingWindow = default__rolling_window(
           aggregation=aggregation,
           columnToAggregate=columnToAggregate,
           partitionColumns=partitionColumns,
@@ -44,8 +44,8 @@ function default__compute_rolling_windows(aggregations, columnsToAggregate, part
           frameSecondsStart=frameSecondsStart,
           frameSecondsEnd=frameSecondsEnd,
         );
-        castAggregationEnd = cast_aggregation_end(aggregation);
-        column = `${aggregation.toLowerCase()}_${columnToAggregate.toLowerCase()}_by_${partitionColumnsName}_${lookbackWindowStart}_${lookbackWindowEnd}`;
+        let castAggregationEnd = cast_aggregation_end(aggregation);
+        let column = `${aggregation.toLowerCase()}_${columnToAggregate.toLowerCase()}_by_${partitionColumnsName}_${lookbackWindowStart}_${lookbackWindowEnd}`;
         result += `${castAggregationStart}${rollingWindow}${castAggregationEnd} AS ${column},\n`;
 
       }
