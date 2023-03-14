ec0d4a5392f1e446c4c1b06a39eb1951c992ac98
Elia Secchi <eliasecchi@google.com>
2023-03-14 16:54:52 +0000

    fix notebooks

--- features/dbt_project/macros/incremental_update_macro.sql
+++ features/dbt_project/macros/incremental_update_macro.sql
@@ -9,13 +9,13 @@ Macro that filters out already processed data in an incremental table.
 
 Args:
 source_column_name [Date/Datetime/Timestamp] : column of the data being modelled that will be used to find the delta
-target_col_name (datetime / date) :  column from the target table that will be used to find the delta
+target_column_name (datetime / date) :  column from the target table that will be used to find the delta
 relation( string ) : Table/View on which the filter will be applied. It defaults to the actual table
 #}
 {% if is_incremental() %}
 AND (
-    DATE({{source_col_name}}) > (
-    SELECT MAX(DATE({{target_col_name}}))
+    DATE({{ source_column_name }}) > (
+    SELECT MAX(DATE({{ target_column_name }}))
     FROM {% if relation == "this" %} {{ this }} {% else %} {{ relation }} {% endif %}
     )
 )
